e04879a91ba51afb9a711c665502f9930d40e46b
Cedric Champeau <cedric.champeau@gmail.com>
2014-06-02 21:14:33 +0200

    Generate plugin and task files from configuration properties

--- templates/gradle-plugin/lazybones.groovy
+++ templates/gradle-plugin/lazybones.groovy
@@ -1,3 +1,5 @@
+File projectDir = targetDir instanceof File?targetDir:new File(String.valueOf(targetDir))
+
 def params = [:]
 params['groupId'] = ask('What is the groupId for your plugin?\n')
 params['pluginName'] = ask('What is the short name for your plugin?\n')
@@ -7,8 +9,72 @@ params['version'] = ask("What is the project's initial version?\n", '0.1', 'vers
     processTemplates(it, params)
 }
 
-File projectDir = targetDir instanceof File?targetDir:new File(String.valueOf(targetDir))
-File descriptorFile = new File(projectDir, "src/main/resources/META-INF/gradle-plugins/${params.pluginName}.properties")
-descriptorFile.parentFile.mkdirs()
-descriptorFile << 'implementation-class=com.acme.gradle.DummyPlugin'
+String capitalized = params.pluginName.capitalize()
+String shortPluginClassName = "${capitalized}Plugin"
+String shortPluginTaskName = "${capitalized}Task"
+String fqPluginClassName = "${params.groupId}.${shortPluginClassName}"
+
+def createTemplateFile = { String path, String contents ->
+    File f = new File(projectDir, path)
+    f.parentFile.mkdirs()
+    f << contents
+}
+
+createTemplateFile("src/main/resources/META-INF/gradle-plugins/${params.pluginName}.properties", "implementation-class=${fqPluginClassName}")
+
+createTemplateFile("src/main/groovy/${params.groupId.replaceAll('\\.','/')}/${shortPluginClassName}.groovy", """\
+package ${params.groupId}
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * This is the main plugin file. Put a description of your plugin here.
+ */
+class ${shortPluginClassName} implements Plugin<Project> {
+    void apply(Project project) {
+
+        project.task('${params.pluginName}', type:${shortPluginTaskName})
+    }
+}
+""")
+
+createTemplateFile("src/main/groovy/${params.groupId.replaceAll('\\.','/')}/${shortPluginTaskName}.groovy", """\
+package ${params.groupId}
+
+import org.gradle.api.internal.AbstractTask
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * This is a dummy task added by the plugin. Put some information about your
+ * task here.
+ */
+class ${shortPluginTaskName} extends AbstractTask {
+
+    @TaskAction
+    void exec() {
+    }
+
+}
+""")
+
+createTemplateFile("src/test/groovy/${params.groupId.replaceAll('\\.','/')}/${shortPluginClassName}Test.groovy", """\
+package ${params.groupId}
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+class ${shortPluginClassName}Test {
+    @Test
+    void pluginIsApplied() {
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: '${params.pluginName}'
+
+
+        def task = project.tasks.findByName('${params.pluginName}')
+        assert task instanceof ${shortPluginTaskName}
+    }
+}
+""")
 
